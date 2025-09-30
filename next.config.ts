2602ca229df7bfa197468e5624185fa9b3103dd3
nun <chhanutry227@gmail.com>
2025-09-30 12:42:54 -0700

    feat: redeploy

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    // Either remotePatterns or domains, choose what you need
     remotePatterns: [
       {
         protocol: "https",
         hostname: "i.pinimg.com",
       },
     ],
+    domains: ["i.pinimg.com"], // optional if you want the old domains array
   },
 };
 
